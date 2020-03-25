63fed8ef04ab685c6f25ec92aaf2514efe688db1
elcarvalho <lessa.eric@gmail.com>
2020-03-25 20:07:08 -0300

    chore(layout): criando página de cadastro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import "./global.css";
 
-import Logon from "./pages/Logon";
+import Routes from "./routes";
 // import { Container } from './styles';
 
 export default function App() {
-  return <Logon />;
+  return <Routes />;
 }
