fe5ea034e99dda91d1fd2284c4a27b43342dddba
elcarvalho <lessa.eric@gmail.com>
2020-03-25 19:41:47 -0300

    chore(layout): definindo estilos globais e página inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./global.css";
+
+import Logon from "./pages/Logon";
+// import { Container } from './styles';
+
+export default function App() {
+  return <Logon />;
+}
