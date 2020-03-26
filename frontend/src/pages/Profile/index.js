81a1eaef644194024bffc08251bd89765db82709
elcarvalho <lessa.eric@gmail.com>
2020-03-26 10:02:53 -0300

    feat(profile): logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import api from "../../services/api";
@@ -9,6 +9,8 @@ import "./styles.css";
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
 
   const ongId = localStorage.getItem("ongId");
   const ongName = localStorage.getItem("ongName");
@@ -37,6 +39,11 @@ export default function Profile() {
     }
   };
 
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push("/");
+  };
+
   return (
     <div className="profile-container">
       <header>
@@ -46,7 +53,7 @@ export default function Profile() {
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso.
         </Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
